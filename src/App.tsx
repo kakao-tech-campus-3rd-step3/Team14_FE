88245da24db215c0a01dcb55c028c237a44fd7e3
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-08-30 19:51:26 +0900

    refactor: app.css 삭제 후 app.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div className="p-8 text-3xl text-blue-500">
-        Tailwind v4 for festaPICK is ready
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div className="space-y-4 text-center">
+        <div className="p-8 text-3xl text-blue-500">
+          Tailwind v4 for festaPICK is ready
+        </div>
+        <div className="p-8 text-3xl text-blue-300">
+          Tailwind v4 for festaPICK is ready
+        </div>
+        <div className="p-8 text-3xl text-blue-200">
+          Tailwind v4 for festaPICK is ready
+        </div>
       </div>
-      <div className="p-8 text-3xl text-blue-300">
-        Tailwind v4 for festaPICK is ready
-      </div>
-      <div className="p-8 text-3xl text-blue-200">
-        Tailwind v4 for festaPICK is ready
-      </div>
-    </>
+    </div>
   );
 }
 
