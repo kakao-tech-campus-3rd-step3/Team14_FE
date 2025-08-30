879238a19b85720423d86ece7333900bd70f6750
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-08-30 19:45:19 +0900

    chore: @tailwindcss/vite 를 통한 tailwind v4 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="p-8 text-3xl text-blue-500">
+        Tailwind v4 for festaPICK is ready
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="p-8 text-3xl text-blue-300">
+        Tailwind v4 for festaPICK is ready
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="p-8 text-3xl text-blue-200">
+        Tailwind v4 for festaPICK is ready
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
