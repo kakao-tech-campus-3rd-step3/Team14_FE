f1d6326999bee7bb855caa39c91cf303c325ade7
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-09-07 12:53:39 +0900

    chore: 의존성 파일 정리

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,8 +22,6 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   extends: [
-    'airbnb',
-    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
