6e77835039b7a080eb4750bd737644460998c2cc
dib3474 <dib3474@gmail.com>
2025-09-11 20:16:51 +0900

    feat: FestivalsPage API 연결

---
+++ src/apis/festivals/getFestivals.ts
@@ -0,0 +1,44 @@
+import { apiInstance } from '@/apis/instance';
+import API_ENDPOINTS from '@/constants/apiEndpoints';
+import type { Festival } from '@/types/FestivalType';
+import type { ApiErrorResponse } from '@/apis/instance';
+import type { AxiosResponse } from 'axios';
+import { generatePath } from 'react-router-dom';
+
+interface GetFestivalsResponse {
+  content: Festival[];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+}
+
+export const getFestivals = async (params: {
+  areaId: string;
+}): Promise<AxiosResponse<GetFestivalsResponse, ApiErrorResponse>> => {
+  return await apiInstance.get<GetFestivalsResponse>(
+    generatePath(API_ENDPOINTS.FESTIVALS, { areaId: params.areaId }),
+  );
+};
+
+export default getFestivals;
