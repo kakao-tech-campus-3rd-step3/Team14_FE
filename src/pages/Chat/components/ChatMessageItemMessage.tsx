bbecf3f47a9f0b9798a15c26099d2ef1cc96a8ce
dib3474 <dib3474@gmail.com>
2025-10-08 01:22:01 +0900

    chore: 오타 수정

--- src/pages/Chat/components/ChatMessageItemMessage.tsx
+++ src/pages/Chat/components/ChatMessageItemMessage.tsx
@@ -1,7 +1,7 @@
 const ChatMessageItemMessage = ({ message, isSelf }: { message: string; isSelf: boolean }) => {
   return (
     <div
-      className={`inline-block max-w-[80vw] rounded-lg px-3 py-2 text-sm whitespace-pre-wrap break-words ${isSelf ? 'bg-primary-100 text-gra-900' : 'bg-gray-100 text-gray-900'}`}
+      className={`inline-block max-w-[80vw] rounded-lg px-3 py-2 text-sm whitespace-pre-wrap break-words ${isSelf ? 'bg-primary-100 text-gray-900' : 'bg-gray-100 text-gray-900'}`}
     >
       {message}
     </div>
