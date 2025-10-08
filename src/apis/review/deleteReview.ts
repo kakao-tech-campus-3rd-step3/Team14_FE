a3b830d5a9bcec7a27c53e769b2bb9cd2fa7c9d3
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-10-08 15:45:09 +0900

    refactor: 엔드포인트 수정

--- src/apis/review/deleteReview.ts
+++ src/apis/review/deleteReview.ts
@@ -1,13 +1,14 @@
 import { apiInstance } from '@/apis/apiInstance';
+import API_ENDPOINTS from '@/constants/apiEndpoints';
+
 /**
  * 리뷰 삭제를 위한 함수
- * 쿼리 파라미터를 사용하지 않고 직접 경로를 사용하여 삭제를 구현하였습니다.
- * 수정 전, 엔드포인트 추가 후 params 사용한 방식에서 URL치환 안되는 이슈 발생 -> 추후 적절한 방식으로 다시 수정하겠습니다.
- * @param reviewId
- * @returns
+ * @param reviewId 삭제할 리뷰 ID
+ * @returns 삭제 결과
  */
-
 export const deleteReview = async (reviewId: number) => {
-  const response = await apiInstance.delete(`/api/reviews/${reviewId}`);
+  const response = await apiInstance.delete(
+    API_ENDPOINTS.REVIEW_DELETE.replace(':reviewId', reviewId.toString())
+  );
   return response.data;
 };
