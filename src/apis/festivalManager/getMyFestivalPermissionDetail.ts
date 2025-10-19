1f7e301e0bf47740e67ecad19492d4c658fc3c28
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-10-20 04:36:02 +0900

    feat: 신청목록 상세 함수 구현

---
+++ src/apis/festivalManager/getMyFestivalPermissionDetail.ts
@@ -0,0 +1,25 @@
+import { apiInstance } from '@/apis/apiInstance';
+import API_ENDPOINTS from '@/constants/apiEndpoints';
+import { generatePath } from 'react-router-dom';
+import type { AxiosResponse } from 'axios';
+import type { ApiResponseItem } from '@/apis/apiResponse'; 
+import type { ApplicationState } from '@/apis/festivalManager/getMyFestivalPermissions';
+
+export interface FestivalPermissionDetail {
+  id: number;
+  title: string;
+  posterImg: string;
+  appliedDate: string;
+  state: ApplicationState;
+  docs: string[];
+}
+
+export type FestivalPermissionDetailResponse = ApiResponseItem<FestivalPermissionDetail>;
+
+export const getFestivalPermissionDetail = async (
+  id: string
+): Promise<AxiosResponse<FestivalPermissionDetailResponse>> => {
+  return await apiInstance.get<FestivalPermissionDetailResponse>(
+    generatePath(API_ENDPOINTS.FESTIVAL_PERMISSION_DETAIL, { id })
+  );
+};
