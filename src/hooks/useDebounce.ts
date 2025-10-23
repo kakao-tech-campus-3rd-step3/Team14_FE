88497afb34ef6d921e34ab3471ddbcdbc966c7d2
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-10-23 22:15:32 +0900

    chore: debounce주석 추가

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from 'react';
-
+/**
+ * 디바운스 훅
+ * 검색 기능 시 디바운스를 구현하기 위한 훅입니다.
+ * 디바운스를 적용하여 매번 요청하지 않도록 하지만 사용자가 입력할 때 일정시간마다 뜨게함으로써 나은 사용자경험을 의도하였습니다.
+ * @param value - 디바운스할 값
+ * @param delay - 디바운스 지연 시간
+ * @returns 디바운스된 값
+ */
 const useDebounce = <T>(value: T, delay: number): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
   const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
