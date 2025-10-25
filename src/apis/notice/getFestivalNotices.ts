9105cbe04be61a952d3dc7dca8f7cb8b6aab5b7c
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-10-25 16:39:53 +0900

    feat: 축제 기준 공지사항 조회 함수 구현

---
+++ src/apis/notice/getFestivalNotices.ts
@@ -0,0 +1,29 @@
+import { apiInstance } from '@/apis/apiInstance';
+import API_ENDPOINTS from '@/constants/apiEndpoints';
+import type { AxiosResponse } from 'axios';
+import type { ApiResponseList } from '@/apis/apiResponse';
+import { generatePath } from 'react-router-dom';
+
+export interface FestivalNoticeItem {
+  festivalId: string;
+  page: number;
+  size: number;
+}
+/**
+ * 축제를 기준으로 공지사항 목록을 조회하는 API 함수입니다.
+ * @param festivalId 축제 ID
+ * @param page 페이지 번호
+ * @param size 페이지 크기
+ * @returns 축제 공지사항 목록
+ */
+export const getFestivalNotices = async (
+  festivalId: string,
+  page: number = 0,
+  size: number = 5,
+): Promise<AxiosResponse< ApiResponseList<FestivalNoticeItem>>> => {
+  return await apiInstance.get< ApiResponseList<FestivalNoticeItem>>(
+    generatePath(API_ENDPOINTS.FESTIVAL_NOTICE, { festivalId }),
+    {
+    params: { page, size },
+  });
+};
