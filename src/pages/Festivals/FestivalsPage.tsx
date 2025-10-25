28e312497cf7b97ab1f664eedbfc863be5889508
dib3474 <dib3474@gmail.com>
2025-10-26 01:35:51 +0900

    feat: FestivalsPage EerrorBoundary 변경

--- src/pages/Festivals/FestivalsPage.tsx
+++ src/pages/Festivals/FestivalsPage.tsx
@@ -6,18 +6,33 @@ import FestivalsAreaSection from '@/pages/Festivals/components/FestivalsAreaSect
 import { Suspense } from 'react';
 import { FestivalsSectionSkeleton } from '@/pages/Festivals/components/FestivalCardSkeleton';
 import { ErrorBoundary } from 'react-error-boundary';
+import EmptyComponent from '@/components/common/EmptyComponent';
 
 const FestivalsPage = () => {
   return (
     <Container>
       <Header variant="page" />
       <div className="flex flex-col items-center px-8 py-4 gap-12">
-        <ErrorBoundary FallbackComponent={() => null}>
+        <ErrorBoundary
+          FallbackComponent={() => (
+            <EmptyComponent
+              title="AI 추천 데이터를 불러오는 중 오류가 발생했습니다."
+              description="잠시 후 다시 시도해주세요."
+            />
+          )}
+        >
           <Suspense fallback={<FestivalsSectionSkeleton />}>
             <FestivalsAISection />
           </Suspense>
         </ErrorBoundary>
-        <ErrorBoundary FallbackComponent={() => null}>
+        <ErrorBoundary
+          FallbackComponent={() => (
+            <EmptyComponent
+              title="지역별 축제 데이터를 불러오는 중 오류가 발생했습니다."
+              description="잠시 후 다시 시도해주세요."
+            />
+          )}
+        >
           <Suspense fallback={<FestivalsSectionSkeleton />}>
             <FestivalsAreaSection />
           </Suspense>
