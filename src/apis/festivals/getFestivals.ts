abe50ab5032108417f59c12773817cf20b405356
dib3474 <dib3474@gmail.com>
2025-10-31 00:32:52 +0900

    refactor: getFestivals 기본 값 구조 변경

--- src/apis/festivals/getFestivals.ts
+++ src/apis/festivals/getFestivals.ts
@@ -6,14 +6,18 @@ import type { ApiErrorResponse } from '@/apis/apiInstance';
 import type { AxiosResponse } from 'axios';
 import { generatePath } from 'react-router-dom';
 
-export const getFestivals = async (params: {
+export const getFestivals = async ({
+  areaId,
+  size = 6,
+  page = 0,
+}: {
   areaId: string;
-  size: number;
-  page: number;
+  size?: number;
+  page?: number;
 }): Promise<AxiosResponse<ApiResponseList<Festival>, ApiErrorResponse>> => {
   return await apiInstance.get<ApiResponseList<Festival>>(
-    generatePath(API_ENDPOINTS.FESTIVALS, { areaId: params.areaId }),
-    { params: { size: params.size ?? 5, page: params.page ?? 0 } as const },
+    generatePath(API_ENDPOINTS.FESTIVALS, { areaId }),
+    { params: { size, page } },
   );
 };
 
