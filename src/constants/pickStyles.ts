1c30fd3157b1f88bbd7f22c0a73181a823b31f4a
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-11-02 23:00:56 +0900

    chore: svg 파일 및 스타일 상수 변경

--- src/constants/pickStyles.ts
+++ src/constants/pickStyles.ts
@@ -1,13 +1,13 @@
 const PICK_STYLES = [
   { id: 'TRADITIONAL', name: '전통 문화', image: '/pickStyle/tradition.svg' },
   { id: 'ART_PERFORMANCE', name: '예술/공연', image: '/pickStyle/art.svg' },
-  { id: 'FOOD', name: '먹거리', image: '/pickStyle/food.svg' },
+  { id: 'RESTING', name: '휴식', image: '/pickStyle/resting.svg' },
   { id: 'NATURE', name: '자연/경관', image: '/pickStyle/nature.svg' },
-  { id: 'EXPERIENCE', name: '체험/참여', image: '/pickStyle/experience.svg' },
-  { id: 'TRENDY', name: '트렌디', image: '/pickStyle/trendy.svg' },
-  { id: 'COMMUNITY', name: '커뮤니티', image: '/pickStyle/community.svg' },
+  { id: 'ACTIVITY', name: '체험/참여', image: '/pickStyle/activity.svg' },
+  { id: 'CITY', name: '도시', image: '/pickStyle/city.svg' },
+  { id: 'PHOTOSHOT', name: '사진촬영', image: '/pickStyle/photoshot.svg' },
   { id: 'LOCAL', name: '지역특색', image: '/pickStyle/local.svg' },
-  { id: 'INTERNATIONAL', name: '국제', image: '/pickStyle/international.svg' },
+  { id: 'FUNEXPERIENCE', name: '짜릿한 경험', image: '/pickStyle/funexperience.svg' },
 ] as const;
 
 // PICK_STYLES의 id 값들만 허용하는 타입
