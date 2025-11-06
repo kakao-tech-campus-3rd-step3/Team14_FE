68937ef3285a8fef8961aa83956e27c74cbec046
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-11-06 10:48:20 +0900

    fix: TextInputWithCounter로 글자수 제한

--- src/components/festivalForm/FestivalFormNameCard.tsx
+++ src/components/festivalForm/FestivalFormNameCard.tsx
@@ -1,8 +1,10 @@
 import BorderCardComponent from '@/components/common/BorderCardComponent';
 import type { FestivalCardProps } from '@/types/FestivalFormTypes';
+import TextInputWithCounter from '../form/TextInputWithCounter';
 
 /**
  * 축제 등록,수정 폼 제목 카드
+ * 축제 제목은 최대 255자까지 입력 가능합니다.
  * @param formData - 폼 데이터
  * @param handleInputChange - 입력 변경 핸들러
  * @param isSubmitting - 제출 중 여부
@@ -16,14 +18,13 @@ const FestivalFormNameCard = ({ formData, handleInputChange, isSubmitting }: Fes
         <label htmlFor="title" className="font-semibold">
           축제 제목 <span className="text-red-500">*</span>
         </label>
-        <input
-          id="title"
-          name="title"
-          type="text"
+        <TextInputWithCounter
           value={formData.title}
           onChange={handleInputChange}
           placeholder="축제 제목을 입력하세요"
-          className="border border-gray-200 rounded-lg p-3"
+          maxLength={255}
+          type="input"
+          name="title"
           disabled={isSubmitting}
         />
       </div>
